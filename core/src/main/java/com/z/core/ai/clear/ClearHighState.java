b222f10eb7adfb9a4513ed4081bc80bab417fb3f
checkcode007 <zcj007w@163.com>
2025-02-20 19:54:37 +0800

    新概率-2

--- core/src/main/java/com/z/core/ai/clear/ClearHighState.java
+++ core/src/main/java/com/z/core/ai/clear/ClearHighState.java
@@ -25,18 +25,6 @@ public class ClearHighState extends ClearState {
     public Map<Integer, Integer> weight(Map<Integer, Slot> slots, List<Slot> list, Set<Integer> goals, BetParam param) {
         Map<Integer, Integer> map = new HashMap();
         for (Slot s : list) {
-            // 降低的概率
-            boolean isGoal = goals != null && goals.contains(s.getK());
-            // 动态调整权重变化：目标符号增加的幅度比非目标符号小
-            int adjustFactor = isGoal ? diffW1 : (int) (diffW1 * 0.5); // 目标符号调整幅度小于非目标符号
-            if (s.isScatter()){
-                adjustFactor = isGoal ? diffW1 * 3 : diffW1 /3 ;
-            }
-            if (isGoal) {
-                s.subW1(adjustFactor);
-            } else {
-                s.addW1(adjustFactor);
-            }
             map.put(s.getK(), s.getW1());
         }
         freeWeight( map,slots, param);
--- core/src/main/java/com/z/core/ai/clear/ClearLowState.java
+++ core/src/main/java/com/z/core/ai/clear/ClearLowState.java
@@ -28,15 +28,7 @@ public class ClearLowState extends ClearState {
     public Map<Integer, Integer> weight(Map<Integer, Slot> slots, List<Slot> list, Set<Integer> goals, BetParam param) {
         Map<Integer, Integer> map = new HashMap();
         for (Slot s : list) {
-            // 降低的概率
-            boolean isGoal = goals != null && goals.contains(s.getK());
-            // 动态调整权重变化：目标符号增加的幅度比非目标符号小
-            int adjustFactor = isGoal ? diffW1 : (int) (diffW1 * 0.2); // 目标符号调整幅度小于非目标符号
-            if (isGoal) {
-                s.subW1(adjustFactor);
-            } else {
-                s.addW1(adjustFactor);
-            }
+
             map.put(s.getK(), s.getW1());
         }
         freeWeight(map, slots, param);
